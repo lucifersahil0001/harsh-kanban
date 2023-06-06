5dc20253217582723cc55d06085df4e401eac0d7
aayush <ayushwasnik999@gmail.com>
2023-06-06 12:16:42 +0530

    navbar added

--- Kanban-Project/src/Components/organism/NavBar/NavBar.jsx
+++ Kanban-Project/src/Components/organism/NavBar/NavBar.jsx
@@ -5,7 +5,10 @@ import Button from "@mui/material/Button";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import StarIcon from "@mui/icons-material/Star";
 import PeopleIcon from "@mui/icons-material/People";
-import HomeIcon from "@mui/icons-material/Home";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import FilterListOutlinedIcon from "@mui/icons-material/FilterListOutlined";
+import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
 
 function NavBar() {
   const [star, setStar] = useState(true);
@@ -44,9 +47,27 @@ function NavBar() {
           <Button
             variant="text"
             sx={{ color: "white", textTransform: "none" }}
-            startIcon={<HomeIcon />}
+            startIcon={<RocketLaunchIcon />}
           >
-            Home
+            Power-Ups
+          </Button>
+
+          <Button
+            variant="text"
+            sx={{ color: "white", textTransform: "none" }}
+            startIcon={<FlashOnIcon />}
+          >
+            Automation
+          </Button>
+          <Button
+            variant="text"
+            sx={{ color: "white", textTransform: "none" }}
+            startIcon={<FilterListOutlinedIcon />}
+          >
+            Filter
+          </Button>
+          <Button sx={{ color: "white" }}>
+            <MoreHorizOutlinedIcon />
           </Button>
         </div>
       </nav>
